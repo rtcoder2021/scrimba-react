2d882f5863481ac1defbe80ea6d4cb978bffb251
rtcoder2021 <91818963+rtcoder2021@users.noreply.github.com>
2023-05-04 15:52:13 -0500

    added Main secion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import Header from './components/Header';
+import Main from './components/Main';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='App-main'>
+      <h1>Fun Facts About React</h1>
+      <ul className=''>
+        <li>Was first released in 2013</li>
+        <li>Was Originally created by Jordan Walke</li>
+        <li>Has over 100k stards on Github</li>
+        <li>Is Maintained by Facebook</li>
+        <li>Powers thousands of Enterprise apps</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Main
